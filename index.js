ac856c7a2b3b198cb7fabe872af91a8e0685c1e2
chetanlohani4321 <chetanlohani310@gmail.com>
2024-03-16 20:41:46 +0530

    changes

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const cors = require('cors');
 
 // Connection URI
 const DB = "mongodb+srv://root:MongoDb%40123!@cluster0.8ujyaqs.mongodb.net/Ticketing"
-mongoose.connect(DB, {
-  useNewUrlParser: true,
-  //useCreateIndex: true,
-  useUnifiedTopology: true,
-  //useFindAndModify: false
-}).then(() => {
+mongoose.connect(DB)
+//    {
+//   useNewUrlParser: true,
+//   useCreateIndex: true,
+//   useUnifiedTopology: true,
+//   useFindAndModify: false
+// })
+.then(() => {
   console.log("Connection successful");
 }).catch((err) => console.log("No connection"));
 
